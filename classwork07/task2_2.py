35876e565cec4b8657ed3d83df921c3609c2b8ea
temochka2005 <glaid2004@gmail.com>
2022-11-04 16:42:05 +0300

    Initial commit, classwork07

--- classwork07/task2_2.py
+++ classwork07/task2_2.py
@@ -2,4 +2,3 @@ import pandas as pd
 diamonds = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
 
 print(diamonds.select_dtypes(include=['float64','int64']))
-d
