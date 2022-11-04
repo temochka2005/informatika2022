cdf6ef775390f4032311507fd7d53328a354b45a
temochka2005 <glaid2004@gmail.com>
2022-11-04 16:55:19 +0300

    Initial commit, classwork07

--- classwork07/task2_9.py
+++ classwork07/task2_9.py
@@ -3,6 +3,6 @@ import numpy as np
 import pandas as pd
 diamonds = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
 def func():
-    i=np.sort(random.sample(range(len(diamonds)), 20))
+    i = np.sort(random.sample(range(len(diamonds)), 20))
     return diamonds.iloc[i]
 print(func())
